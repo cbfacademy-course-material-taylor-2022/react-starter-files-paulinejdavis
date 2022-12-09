2570457429318d68230626f77f832acbeae1db77
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-12-09 12:28:48 +0000

    added funcitonality for any name or no name

--- jsx-components/task01/src/index.js
+++ jsx-components/task01/src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const name = 'Pauline';
+const names = 'Pauline'
 
 function Profile(name){
+  if (name.toUpperCase() === names.toUpperCase()){
     return <main id="content" role="main" className="base">
-            <h1>Pauline's React Page</h1>
-              <p>Here is my bio. Isn't it great.</p>
-            </main>
+    <h1>{name}'s' React Page</h1>
+      <p>Here is my bio. Isn't it great.</p>
+    </main>
+  } else if (name != "") {
+    return <main id="content" role="main" className="base">.
+    <h1>{name}'s' React Page</h1>
+      <p>This is JSX in action.</p>
+    </main>
+  } else {
+    return <main id="content" role="main" className="base">
+    <h1>This is a React Page</h1>
+      <p>Not much going on</p>
+    </main>
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
