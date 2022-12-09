ac3da0b16cfc38866d329d7de02370184158de8f
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-12-09 14:15:24 +0000

    added useState functionality

--- props-and-state/src/index.js
+++ props-and-state/src/index.js
@@ -1,17 +1,19 @@
 import React, { useState, Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 import data from './models/headphones.json';
+import ClickCounter from './components/ClickCounter'
 
 import GadgetList from './components/GadgetList';
 const mainStyle = {"fontFamily":"Arial"};
 
 const App = () => {
   const headphones =  [{"title":"Headphones 1", "price": 358.99},{"title":"Headphones 2", "price": 358.99},{"title":"Headphones 3", "price": 358.99},{"title":"Headphones 4", "price": 358.99},{"title":"Headphones 5", "price": 358.99}];
-  const [headphonesList] = useState(headphones);
+  const [headphonesList] = useState(data);
 
   return <section style={mainStyle}>
     <h1>Electronics Store</h1>
     <GadgetList items={headphonesList}/>
+    <ClickCounter/>
   </section>;
 }
 
