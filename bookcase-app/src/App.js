dd55b6fb525ae33a40717e25b20bf9ab1eeffa20
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-12-09 14:16:06 +0000

    added bookcase components / lib details

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import Book from './components/Book';
+import React, {useState } from 'react';
+// import Search from './components/search';
+// import Book from './components/Book';
 import data from './models/books.json';
+import BookList from './components/BookList';
+
 
 function App() {
-  const books = data;
-  return <Book/>;
+    const [books] = useState(data);
+
+    function addBook(book) {
+        console.log(`You selected '{book}' to add to your bookcase`);
+    }
+    
+    return <BookList books={books} addBook={addBook} />;
+
 }
-
 export default App;
-
