25c3b1f7e681d9716d1406213d9c816e52680700
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-12-09 12:57:52 +0000

    add state components

--- props-and-state/src/components/GadgetList.js
+++ props-and-state/src/components/GadgetList.js
@@ -5,7 +5,8 @@ const GadgetList = (props) => {
   return (
     <main>
       <h2>Headphones</h2>
-      {props.items.map((item) => (<div>{item.title}</div>))}
+       <img src='../public/imgs/dre_beats1.png' alt='images'/>
+      {props.items.map((item) => (<div>{item.title} {item.price}</div>))}
     </main>
   );
 }
--- props-and-state/src/index.js
+++ props-and-state/src/index.js
@@ -1,12 +1,12 @@
 import React, { useState, Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
-//import data from './models/headphones.json';
+import data from './models/headphones.json';
 
 import GadgetList from './components/GadgetList';
 const mainStyle = {"fontFamily":"Arial"};
 
 const App = () => {
-  const headphones =  [{"title":"Headphones 1"},{"title":"Headphones 2"},{"title":"Headphones 3"},{"title":"Headphones 4"},];
+  const headphones =  [{"title":"Headphones 1", "price": 358.99},{"title":"Headphones 2", "price": 358.99},{"title":"Headphones 3", "price": 358.99},{"title":"Headphones 4", "price": 358.99},{"title":"Headphones 5", "price": 358.99}];
   const [headphonesList] = useState(headphones);
 
   return <section style={mainStyle}>
