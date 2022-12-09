fad25da07be8039a5d041ebbabc794f9a52b3df5
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-12-09 14:13:20 +0000

    updated task lists

--- nesting-and-events/src/index.js
+++ nesting-and-events/src/index.js
@@ -1,53 +1,56 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import TaskContainer from './components/TaskContainer';
-import Task from './components/Task'
+import Task from './components/Task';
 
 const taskList = [
   {"id":"task1","title":"Buy Milk","isComplete":true},
   {"id":"task2","title":"Read a book","isComplete":false},
   {"id":"task3","title":"Early morning run","isComplete":false},
-  {"id":"task4","title":"Drink water","isComplete":false},
-  {"id":"task5","title":"Visit the puppy shelter","isComplete":true},
-  {"id":"task6","title":"Watch Bad Sisters","isComplete":false}
+  {"id":"task4","title":"Go to the gym","isComplete":false},
+  {"id":"task5","title":"Visit museum","isComplete":true},
+  {"id":"task6","title":"Watch TV","isComplete":false}
 ];
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App/>);
 
 
 function App() {
-  
-  //  return (
-  //     <Fragment>
-  //       <h1 key="heading">Task List</h1>
-  //       <TaskContainer>
-  //         {/* Task: Nesting Components - Add you tasks here: */}
-  //       </TaskContainer>
-  //     </Fragment>
-  // );
-  function toggleComplete(id){
-    setTasks(
-      taskList.map(task => {
-        if (task.id === id) {
-          task.isComplete = !task.isComplete;
-        }
-        return task;
-      })
-    );
-  }
-  const [tasks, setTasks] = useState(taskList);
+//    return (
+//       <Fragment>
+//         <h1 key="heading">Task List</h1>
+//         <TaskContainer>
+//           {/* Task: Nesting Components - Add you tasks here: */}
+//         </TaskContainer>
+//       </Fragment>
+//   );
+// }
+      function toggleComplete(id){
+        setTasks(
+          taskList.map(task => {
+            if (task.id === id) {
+              task.isComplete = !task.isComplete;
+            }
+            return task;
+          })
+        );
+      }
+      const [tasks, setTasks] = useState(taskList);
 
-  return (
-    <Fragment>
-      <h1 key="heading">Task List</h1>
-      <TaskContainer>
-        {tasks.sort((a,b) => (a.isComplete  - b.isComplete))
-              .map(task => 
-                <Task key={task.id} toggleComplete={toggleComplete} id={task.id} title={task.title} isComplete={task.isComplete}/>
-              )}
-        {/*<Task id="task1" title="Buy Milk" isComplete={true}/>
-        <Task id="task2" title="Read a book" isComplete={false}/>
-        <Task id="task3" title="Early morning run" isComplete={false}/>*/}
-      </TaskContainer>
-    </Fragment>
+      return (
+        <Fragment>
+          <h1 key="heading">Task List</h1>
+          <TaskContainer>
+            {tasks.sort((a,b) => (a.isComplete  - b.isComplete))
+                  .map(task => 
+                    <Task key={task.id} toggleComplete={toggleComplete} id={task.id} title={task.title} isComplete={task.isComplete}/>
+                  )}
+            {/*<Task id="task1" title="Buy Milk" isComplete={true}/>
+            <Task id="task2" title="Read a book" isComplete={false}/>
+            <Task id="task3" title="Early morning run" isComplete={false}/>*/}
+          </TaskContainer>
+        </Fragment>
 
 );
 
@@ -55,3 +58,5 @@ function App() {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>)
+
+
