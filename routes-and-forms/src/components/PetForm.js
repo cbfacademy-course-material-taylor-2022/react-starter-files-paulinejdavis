e30f018f2e4ee111ed79cf863ad0988e9f7baa17
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-12-09 16:42:47 +0000

    added age and colours to petform

--- routes-and-forms/src/components/PetForm.js
+++ routes-and-forms/src/components/PetForm.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 const PetForm = () => {
   const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [colours, setColours] = useState('');
   const [,setSubmitted] = useState(false);
 
   function handleSubmit(event) {
@@ -16,12 +18,16 @@ const PetForm = () => {
       <form onSubmit={(e) => handleSubmit(e)}>
         <div>
           <label>Pet's Name:<input type="text" value={name} onChange={(e) => { setName(e.target.value); setSubmitted(false) }} /></label>
+          <label>Age:<input type="text" value={age} onChange={(e) => { setAge(e.target.value); setSubmitted(false) }} /></label>
+          <label>Colours:<input type="text" value={colours} onChange={(e) => { setColours(e.target.value); setSubmitted(false) }} /></label>
         </div>
         <input type="submit" value="Submit" id="submit-button"/>
       </form>
       <div id="results">
         <h3>Results</h3>
             Name: {name}<br />
+            Age: {age}<br />
+            Colours: {colours}<br />
       </div>
     </div>
   );
--- routes-and-forms/src/index.js
+++ routes-and-forms/src/index.js
@@ -1,6 +1,7 @@
 import {React} from "react";
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom'
+import PetForm from "./components/PetForm"
 
 export default function App() {
   return (
