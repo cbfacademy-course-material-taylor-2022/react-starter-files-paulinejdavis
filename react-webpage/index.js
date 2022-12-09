72f2670b5681fbd3003ce857d9aca05d1742d4e8
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-12-09 11:02:05 +0000

    add React to a web page

---
+++ react-webpage/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function ClickableButton() {
+
+  const [clicked, setClick] = React.useState(false);
+
+  if (clicked) {
+    return React.createElement(
+        'h1',
+        {className: 'main-button'},
+        'Hello, World'
+      );
+  }
+
+  return React.createElement(
+    'button', {
+        className: 'main-button',
+    onClick: () => setClick(true)
+  },
+    'Press Here'
+  );
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(React.createElement(ClickableButton));
